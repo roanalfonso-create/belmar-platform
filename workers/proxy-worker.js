608bb349351039f4a1ac00ede00ff7ae0f1cfd44
roanalfonso-create <roan.alfonso@belmarcloud.com>
2026-05-26 15:50:47 -0700

    Updates

--- workers/proxy-worker.js
+++ workers/proxy-worker.js
@@ -24,6 +24,8 @@ const ALLOWED_ORIGINS = [
   "http://localhost",
   "http://localhost:3000",
   "http://127.0.0.1",
+  "https://belmar-platform.pages.dev",  // ← add this
+
 ];
 
 function corsHeaders(origin) {
